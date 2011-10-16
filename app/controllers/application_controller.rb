599e2acec3b719086438f4ec37c95b0e9c17df54
GavinMcD <gavin@brightmonk.com>
2011-10-16 10:08:19 -0700

    Added an authentication method to app_con

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  def authenticate
+    unless current_user
+      redirect_to sign_in_path, :notice => "Please sign in first"
+      return false
+    end
+  end
+  
   private
   
   def current_user
